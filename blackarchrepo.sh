c85bfe0d551208236dccfeba1a485674a99e33c1
hostilesquirrel <michaelmunoka@yahoo.com>
2020-09-24 07:41:57 +0300

    first commit to the help malibu

---
+++ blackarchrepo.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+curl -O https://blackarch.org/strap.sh
+
+chmod +x strap.sh
+
+./strap.sh
+
+rm -rf strap.sh
+
+echo "Thank You for using help malibu scripts"
---
+++ readme.md
@@ -0,0 +1,4 @@
+### Scripts and Uses
+
+### blackarchrepo
+<i>is a script to help arch linux users run and install arch linux repo</i>
