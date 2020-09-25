54ebb81eadbbe1a42ff0d63a19d16a825e7bf217
hostilesquirrel <michaelmunoka@yahoo.com>
2020-09-25 22:50:08 +0300

    added new script

---
+++ ohmyzshonarch.sh
@@ -0,0 +1,8 @@
+echo "Zsh themes will refuse to work sometimes"
+echo "This script will remove grml-zsh-config to make it work"
+echo "Open new terminal after complition"
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+yay -Rncs grml-zsh-config
+
+
