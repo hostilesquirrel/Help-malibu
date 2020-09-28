70ff72570f792544e3a07a6ca740ec9129d548f4
malibu <malibu@localhost.localdomain>
2020-09-28 03:14:11 -0400

    Experimental automation script

---
+++ automate.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+
+#This script is intended to make the installateion of my scripts easier
+
+mkdir ~/.automalibu
+cd ~/.automalibu
+git clone https://github.com/hostilesquirrel/mikeylinuxep
+
+# Make folder on to wich to install the launcher
+touch launcher.sh
+
+echo """
+#! /usr/bin/bash
+cd ~/.automalibu/mikeylinuxep
+
+echo """HELLO THERE, HERE ARE SOME SEVERAL SCRIPTS TO HELP YOU CHOOSE 1\n
+1. fix boot order
+
+
+
+"""
+
+
+""" >> launcher.sh
+
+ 
