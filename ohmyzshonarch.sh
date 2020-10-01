261a9d25247454db3669f359a2e3429720ac7737
hostilesquirrel <michaelmunoka@yahoo.com>
2020-10-01 12:43:17 +0300

    making the script look more mature

--- ohmyzshonarch.sh
+++ ohmyzshonarch.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
 
-echo "Zsh themes will refuse to work sometimes"
-echo "This script will remove grml-zsh-config to make it work"
-echo "Open new terminal after complition"
 
+echo " [◉]  ZSH themes refuse to work on Arch Sometimes"
+echo "	[◉] This script is going to remove grml-zsh-config to make things better"
+sleep 1
+echo "[....] Checking internet connection"
+
+echo "	[◉] Do you want to reinstall ohmyzsh properly y/n"
+read bazu
+
+if [ "$bazu" -eq "y" ];then
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 yay -Rncs grml-zsh-config
+elif [ "$bazu" -eq "n" ];then
+yay -Rncs grml-zsh-config
+else
+	echo "re think your life"
+fi
 
 
