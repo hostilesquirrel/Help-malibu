0502564e13c08e85a6e7118df06c22893f83f0d3
hostilesquirrel <michaelmunoka@yahoo.com>
2020-10-20 03:34:11 +0300

    codeblocks segmentation fault error launching
    On launch codeblocks automatically stops, script stops that error

---
+++ pacman/codeblockssegfault.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo " Solution for Segmentation fault codeblocks-arch "
+sleep 1
+
+echo " Select GNU/GCC and set default on the pop up that appears/ else run as sudo"
+codeblocks --rebuild gnu/gcc
